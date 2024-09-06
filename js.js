872dbfd35f979cad658ef0efc67262e64ee45e3c
insta-stack <83533297+insta-stack@users.noreply.github.com>
2024-09-06 22:02:44 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,13 @@
+let thu = new Date();
+// console.log(thu) 
+//  let jk= thu.tolocalString;
+ let  toLocalStringjk= thu.toLocaleString();//perfect time && Date
+
+// formatted data 
+// todo
+// Both date && time has its own get && set Methode; for hrs week min sec month day millisec
+console.log(toLocalStringjk)
+let date = thu.toDateString();
+console.log(date)
+let time = thu.toLocaleTimeString();
+console.log(time);
